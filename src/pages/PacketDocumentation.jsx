1165fa4661ed600c05728f8c029ce9b0c95a4611
MHamdanHariri20 <m.hamdanhariri5@gmail.com>
2024-10-08 16:17:20 +0700

    add tag google for google ads

--- src/pages/PacketDocumentation.jsx
+++ src/pages/PacketDocumentation.jsx
@@ -7,20 +7,20 @@ import React, { useEffect } from "react";
 export default function PacketDocumentation(){
 
     useEffect(() => {
+        // Buat elemen script untuk memasukkan Google Tag Manager
         const script = document.createElement("script");
-        script.async = true;
-        script.src = `https://www.googletagmanager.com/gtm.js?id=G-2BXBXXM0H5`;
+        script.innerHTML = `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id=G-2BXBXXM0H5';f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','G-2BXBXXM0H5');
+        `;
         document.head.appendChild(script);
     
-        const noscript = document.createElement("noscript");
-        noscript.innerHTML = `
-          <iframe src="https://www.googletagmanager.com/ns.html?id=G-2BXBXXM0H5"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
-        document.body.appendChild(noscript);
-    
+        // Membersihkan script ketika komponen dilepas (unmount)
         return () => {
           document.head.removeChild(script);
-          document.body.removeChild(noscript);
         };
       }, []);
 
