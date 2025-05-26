f2ca1a20206f500b4deab599887f00a48a084540
timmdkrtf <contact@mdkrtf.co.id>
2025-05-26 16:16:53 +0700

    add logo

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -15,6 +15,11 @@ import TMC from "../assets/img/1. TMC Logo Primary (P).png";
 import TMF from "../assets/img/1. TMF Logo Primary (P).png";
 import aka from "../assets/img/AKA.png";
 import ask from "../assets/img/ASK.png";
+import skina from "../assets/img/logo skina.png";
+import cimandala from "../assets/img/Logo_SDN_Cimandala_03.png";
+import kittun from "../assets/img/kittun.png";
+import warungHagia from "../assets/img/warung-hagia.png";
+import kopiJalpin from "../assets/img/kopi-jalpin.png";
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
@@ -89,16 +94,21 @@ const Client = () => {
                   }}
                 className='items-center justify-center'>
                 <SwiperSlide className="apostrof"><img src={apostrof} alt="apostrof" /></SwiperSlide>
+                <SwiperSlide className="apostrof"><img src={cimandala} alt="cimandala" /></SwiperSlide>
                 <SwiperSlide className="bahana"><img src={bahama} alt="bahana" /></SwiperSlide>
                 <SwiperSlide className="habatour"><img src={habatour} alt="habatour" /></SwiperSlide>
+                <SwiperSlide className="habatour"><img src={skina} alt="skina" /></SwiperSlide>
                 <SwiperSlide className="halakah"><img src={halakah} alt="halakah" /></SwiperSlide>
                 <SwiperSlide className="kronika"><img src={kronika} alt="kronika" /></SwiperSlide>
                 <SwiperSlide className="nyamanin"><img src={nyamanin} alt="nyamanin" /></SwiperSlide>
                 <SwiperSlide className="realtour"><img src={realtour} alt="realtour" /></SwiperSlide>
                 <SwiperSlide className="AYP"><img src={AYP} alt="AYP" /></SwiperSlide>
+                <SwiperSlide className="apostrof"><img src={kopiJalpin} alt="kopi jalan pintas" /></SwiperSlide>
                 <SwiperSlide className="ask"><img src={ask} alt="ask" /></SwiperSlide>
+                <SwiperSlide className="habatour"><img src={kittun} alt="kittun" /></SwiperSlide>
                 <SwiperSlide className="TMC"><img src={TMC} alt="TMC" /></SwiperSlide>
                 <SwiperSlide className="aka"><img src={aka} alt="aka" /></SwiperSlide>
+                <SwiperSlide className="apostrof"><img src={warungHagia} alt="warung hagia" /></SwiperSlide>
                 <SwiperSlide className="TMF"><img src={TMF} alt="TMF" /></SwiperSlide>
             </Swiper>
         </section>
