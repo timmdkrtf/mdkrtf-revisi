d7cd2d300177bbb34d1bf00c2a93a7a840758d7f
timmdkrtf <contact@mdkrtf.co.id>
2025-06-24 11:03:24 +0700

    add some portfolio sosmed

--- src/components/SosmedPortfolio.jsx
+++ src/components/SosmedPortfolio.jsx
@@ -4,6 +4,9 @@ import KronikaSosmed from '../assets/sosmed-portofolio/sosmed-kronika.png';
 import NyamaninSosmed from '../assets/sosmed-portofolio/sosmed-nyamanin.png';
 import RealtourSosmed from '../assets/sosmed-portofolio/sosmed-realtour.png';
 import UstazukiSosmed from '../assets/sosmed-portofolio/sosmed-ustazuki.png';
+import TmfSosmed from '../assets/sosmed-portofolio/sosmed-tmf.png';
+import AypSosmed from '../assets/sosmed-portofolio/sosmed-aypfirm.png';
+import HabatourSosmed from '../assets/sosmed-portofolio/sosmed-habatour.png';
 
 
 export default function SosmedPortfolio() {
@@ -38,6 +41,18 @@ export default function SosmedPortfolio() {
                         <a className="content-image" href="https://www.instagram.com/ustazuki.co.id/" target="_blank">
                             <img src={UstazukiSosmed} alt="Ustazuki" />
                             <p>Ustazuki</p>
+                        </a>
+                        <a className="content-image" href="https://www.instagram.com/tmfoundation.or.id/" target="_blank">
+                            <img src={TmfSosmed} alt="Tmfoundation" />
+                            <p>Tmfoundation</p>
+                        </a>
+                        <a className="content-image" href="https://www.instagram.com/habatour.co.id/" target="_blank">
+                            <img src={HabatourSosmed} alt="Habatour" />
+                            <p>Habatour</p>
+                        </a>
+                        <a className="content-image" href="https://www.instagram.com/aypfirm.id/" target="_blank">
+                            <img src={AypSosmed} alt="Aypfirm" />
+                            <p>Aypfirm</p>
                         </a>
                     </div>
                 </div>
